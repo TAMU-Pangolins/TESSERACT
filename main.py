e142f828fd081610df2a160114b542619154be82
Aldus <aldusv@tamu.edu>
2025-09-21 14:55:25 -0500

    project initialization

---
+++ main.py
@@ -0,0 +1,4 @@
+from meow.densities_retrieval import read_bfmeff_dat, read_ctmeff_dat, read_egsm_dat
+
+
+
---
+++ meow/densities_retrieval.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+# https://www-nds.iaea.org/RIPL-3/
+
+
+def read_bfmeff_dat():
+    return
+
+def read_ctmeff_dat():
+    return
+
+def read_egsm_dat():
+    return
+
