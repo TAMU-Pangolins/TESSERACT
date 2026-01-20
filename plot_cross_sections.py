993a64fbd93ae91856c42bf28d64181bf1ef8cff
chirag167 <chiragrathi16798@gmail.com>
2026-01-20 11:16:08 -0600

    Modified to incorporate integrated cross sections

--- plot_cross_sections.py
+++ plot_cross_sections.py
@@ -7,8 +7,8 @@ from scipy.interpolate import make_interp_spline
 
 #resonance_i = Resonance(1,0.5,0.5,0.5,3.65*10**(-26),6.64*10**(-27),1,1)
 output_dir = 'outputs/22Mg(a,p)25Al/'
-file = [output_dir +'RUN_0/22Mg(a,p)25Al.in',output_dir + 'RUN_1/22Mg(a,p)25Al.in']
-bin_width = [0.1,0.2]
+file = output_dir +'RUN_0/22Mg(a,p)25Al.in'
+#bin_width = [0.1,0.2]
 start_marker = 'Resonant Contribution'
 end_marker = 'Upper Limits of Resonances'
 
@@ -46,37 +46,87 @@ def extract_data(file):
 
     return df
 
+
+
+df = extract_data(file)
+df = df.sort_values(by='Ecm',ascending=True)
+E_cm = df['Ecm']
+gamma_i = df['G1']
+gamma_o = df['G2']
+Jr = df['Jr']
+l1 = df['L1']
+l2 = df['L2']
+L = l1 + l2
+
+
+xs_lst = []
+
+# Read inputs as strings first
+E_min_in = input("Enter minimum energy [MeV] for integration (press Enter for default): ")
+E_min = float(E_min_in) if E_min_in.strip() else np.min(E_cm*10**(-3))
+print(E_min)
+
+E_max_in = input("Enter maximum energy [MeV] for integration (press Enter for default): ")
+E_max = float(E_max_in) if E_max_in.strip() else np.max(E_cm*10**(-3))
+print(E_max)
+
+dE_in    = input("Enter bin width [MeV] for integration: ")
+dE    = float(dE_in)
+
+xs_bin = []
+
+N_bins = 50
+
+E_bins = np.arange(E_min,E_max+dE,dE)
+
+for E0 in E_bins:
+    E1 = E0 + dE
+
+    # internal grid (MeV or eV consistently)
+    E_int = np.linspace(E0, E1, N_bins)
+
+    xs_total = np.zeros_like(E_int)
+
+    # sum over resonances
+    for i in range(len(E_cm)):
+        r_i = Resonance(
+            E_cm[i]*10**3,      # resonance energy Er in eV
+            Jr[i],
+            0, 0,
+            3.65e-26,
+            6.64e-27,
+            gamma_i[i],        # Gamma_i at Er (from table)
+            gamma_o[i],        # Gamma_o (from table)
+        )
+
+        xs_r = sigma_bw_energy_dep(
+            E_int*10**6, #eV
+            r_i,
+            12, 2, 22, 4,
+            L[i],
+            gamma2_mev=0.0   # not used since Gamma_i is known
+        )
+
+        xs_total += xs_r
+
+    # integrate over the bin
+    xs_int = np.trapezoid(xs_total, E_int)
+    xs_bin.append(xs_int)
+    print(E0)
+
+#E_lst = np.arange(E_min,E_max+dE,dE)
+
+#for i in range(len(E_cm)):
+    # extracting resonance information
+#    r_i = Resonance(E_cm[i],Jr[i],0,0,3.65*10**(-26),6.64*10**(-27),gamma_i[i],gamma_o[i])
+    # generating cross sections
+#    xs = sigma_bw_energy_dep([E_cm[i]],r_i,12,2,22,4,L[i],0)
+#    xs_lst.append(xs)
+
+
 fig = plt.figure(figsize=(8,6))
 
-for j in range(len(file)):
-
-    df = extract_data(file[j])
-    df = df.sort_values(by='Ecm',ascending=True)
-    E_cm = df['Ecm']
-    gamma_i = df['G1']
-    gamma_o = df['G2']
-    Jr = df['Jr']
-    l1 = df['L1']
-    l2 = df['L2']
-    L = l1 + l2
-
-#resonance_i = Resonance(1,0.5,0.5,0.5,3.65*10**(-26),6.64*10**(-27),1,1)
-
-    xs_lst = []
-    for i in range(len(E_cm)):
-
-        r_i = Resonance(E_cm[i]*10**6,Jr[i],0,0,3.65*10**(-26),6.64*10**(-27),gamma_i[i],gamma_o[i])
-
-        xs = sigma_bw_energy_dep([E_cm[i]*10**6],r_i,12,2,22,4,L[i],0)
-        xs_lst.append(xs)
-
-
-        #plt.scatter(E_cm*10**(-3),xs_ls,s=5)
-    spline_fit = make_interp_spline(E_cm.values,xs_lst)
-    E_spline = np.linspace(E_cm.min(),E_cm.max(),500)
-    xs_spline = spline_fit(E_spline)
-
-    plt.plot(E_spline*10**(-3),xs_spline,lw=2,label=rf"$\Delta$E = {bin_width[j]} MeV")
+plt.plot(E_bins,xs_bin,lw=2,label=rf"$\Delta$E = {dE} MeV")
 
 plt.yscale('log')
 plt.title('22Mg(a,p)25Al')
