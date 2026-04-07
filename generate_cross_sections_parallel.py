c8f31dbea049473f35b96dcc87a86c87faf201eb
Chirag Rathi <chiragrathi16798@gmail.com>
2026-04-07 14:55:36 -0500

    set gamma2 to the incoming channel

--- generate_cross_sections_parallel.py
+++ generate_cross_sections_parallel.py
@@ -299,7 +299,7 @@ def _sigma_worker(args):
             Z1, Z2,
             A1_proj, A1_target,
             res["L"][j],
-            gamma2=res["g2"][j]
+            gamma2=res["g1"][j]
         )
 
         xs_tot += xs_j
